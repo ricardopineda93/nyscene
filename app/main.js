f495639e975bf5790e67bc652d85db373be98290
Ricardo Pineda <richay718@gmail.com>
2019-05-31 00:47:54 -0400

    react component rendering on html page, wepack was successful but had to revert babel to v7 in order to successfully roll everything up together.

--- app/main.js
+++ app/main.js
@@ -5,10 +5,11 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Root from './components/root';
 
-//TODO: Need root component...
-
 render(
   <Provider store={store}>
     <Root />
-  </Provider>
+  </Provider>,
+  document.getElementById('main')
 );
+
+//Always remember to select the DOM element to anchor the component onto!!
