df2056f1af24fc8eda78e0943cb1b21236daeee3
Ricardo Pineda <richay718@gmail.com>
2019-06-15 22:22:06 -0400

    changed db file

--- server/database/db.js
+++ server/database/db.js
@@ -5,8 +5,11 @@ const Sequelize = require('sequelize');
 
 console.log(chalk.yellow('Launching database connection...'));
 
-const db = new Sequelize('postgres://localhost:5432/nyscene', {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/nyscene',
+  {
+    logging: false
+  }
+);
 
 module.exports = db;
